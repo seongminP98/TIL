d5700dd9c1308aec6ddc7b428eb9d02e3653d679
박성민 <psm7335@naver.com>
2023-01-19 14:34:51 +0900

    코틀린에서 제어문을 다루는 방법 (if, when)

---
+++ KotlinStudy/src/codeControl/IfStatement.kt
@@ -0,0 +1,21 @@
+package codeControl
+
+fun getPassOrFail(score: Int): String { // java 의 3항 연산자와 비슷
+    return if (score >= 50) {
+        "P"
+    } else {
+        "F"
+    }
+}
+
+fun getGrade(score: Int): String {
+    return if (score >= 90) {
+        "A"
+    } else if (score >= 80) {
+        "B"
+    } else if (score >= 70) {
+        "C"
+    } else {
+        "D"
+    }
+}
---
+++ KotlinStudy/src/codeControl/SwitchWhenStatement.kt
@@ -0,0 +1,41 @@
+package codeControl
+
+fun getGradeWithSwitch(score: Int): String {
+    return when (score / 10) {
+        9 -> "A"
+        8 -> "B"
+        7 -> "C"
+        else -> "D"
+    }
+}
+
+fun getGradeWithSwitch2(score: Int): String {
+    return when (score) {
+        in 90..99 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+}
+
+fun startsWithA(obj: Any): Boolean {
+    return when (obj) {
+        is String -> obj.startsWith("A")
+        else -> false
+    }
+}
+
+fun judgeNumber(number: Int) {
+    when (number) {
+        1, 0, -1 -> println("어디서 많이 본 숫자입니다.")
+        else -> println("1, 0, -1 이 아닙니다.")
+    }
+}
+
+fun judgeNumber2(number: Int) { // when 을 early return 처럼 활용 가능
+    when {
+        number == 0 -> println("주어진 숫자는 0입니다.")
+        number % 2 == 0 -> println("주어진 숫자는 짝수입니다.")
+        else -> println("주어진 숫자는 홀수입니다.")
+    }
+}
