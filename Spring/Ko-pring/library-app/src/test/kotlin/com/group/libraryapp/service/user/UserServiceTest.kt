3b5549f61594e7154faf2686dc270fc4af43b6f1
박성민 <psm7335@naver.com>
2023-03-20 17:41:04 +0900

    User 도메인계층을 Kotlin으로 변경

--- Spring/Ko-pring/library-app/src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ Spring/Ko-pring/library-app/src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -43,8 +43,8 @@ class UserServiceTest @Autowired constructor(
     fun getUsersTest() {
         // given
         userRepository.saveAll(listOf(
-                User("A", 20),
-                User("B", null),
+            User("A", 20),
+            User("B", null),
         ))
 
         // when
@@ -61,7 +61,7 @@ class UserServiceTest @Autowired constructor(
     fun updateUserNameTest() {
         // given
         val savedUser = userRepository.save(User("A", null))
-        val request = UserUpdateRequest(savedUser.id, "B")
+        val request = UserUpdateRequest(savedUser.id!!, "B")
 
         // when
         userService.updateUserName(request)
