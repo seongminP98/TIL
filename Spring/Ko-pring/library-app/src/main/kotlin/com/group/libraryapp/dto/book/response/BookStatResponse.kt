0724c1c5549e421eb5c2368e8413f35cfc35f2c9
박성민 <psm7335@naver.com>
2023-03-22 17:21:44 +0900

    책 통계 조회 - 테스트 코드, 프로덕션 코드 리팩토링

--- Spring/Ko-pring/library-app/src/main/kotlin/com/group/libraryapp/dto/book/response/BookStatResponse.kt
+++ Spring/Ko-pring/library-app/src/main/kotlin/com/group/libraryapp/dto/book/response/BookStatResponse.kt
@@ -2,8 +2,4 @@ package com.group.libraryapp.dto.book.response
 
 import com.group.libraryapp.domain.book.BookType
 
-data class BookStatResponse(val type: BookType, var count: Int) {
-    fun plusOne() {
-        count++
-    }
-}
+data class BookStatResponse(val type: BookType, val count: Int)
