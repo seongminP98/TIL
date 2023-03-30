6ada5e470092163138f8397d925e693127f9eaac
박성민 <psm7335@naver.com>
2023-03-30 23:12:38 +0900

    Hello 컨트롤러 매핑과 바인딩

--- Spring/토비의 스프링/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Spring/토비의 스프링/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -21,6 +21,7 @@ public class HellobootApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
             servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -28,9 +29,11 @@ public class HellobootApplication {
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        String ret = helloController.hello(name);
+
                         resp.setStatus(HttpStatus.OK.value());
                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println("Hello " + name);
+                        resp.getWriter().println(ret);
                     } else if (req.getRequestURI().equals("/user")) {
                         //
                     } else {
