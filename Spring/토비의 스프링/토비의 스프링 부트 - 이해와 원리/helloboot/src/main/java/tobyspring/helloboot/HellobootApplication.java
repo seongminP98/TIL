7bd7f6f814118ba7e2c2181076cad34d309d92c5
박성민 <psm7335@naver.com>
2023-03-30 21:42:11 +0900

    서블릿 요청 처리

--- Spring/토비의 스프링/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Spring/토비의 스프링/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -9,6 +9,9 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import java.io.IOException;
 
@@ -20,9 +23,11 @@ public class HellobootApplication {
             servletContext.addServlet("hello", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    resp.setStatus(200);
-                    resp.setHeader("Content-Type", "text/plain");
-                    resp.getWriter().println("Hello Servlet");
+                    String name = req.getParameter("name");
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello " + name);
                 }
             }).addMapping("/hello");
         });
