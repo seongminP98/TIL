f0d5d7a7e3b3557291637719a6ecc01bd716df4d
박성민 <psm7335@naver.com>
2023-03-31 23:17:25 +0900

    테스트 코드를 이용한 테스트

---
+++ Spring/토비의 스프링/토비의 스프링 부트 - 이해와 원리/helloboot/src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,26 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class HelloApiTest {
+    @Test
+    void helloApi() {
+        // http localhost:8080/hello?name=Spring
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res
+                = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status code 200
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header(content-type) text/plain
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        //body
+        assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+}
