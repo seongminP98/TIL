a2e20413a09397e75d9edc4f4def308fb8ee144d
박성민 <psm7335@naver.com>
2023-03-31 22:18:34 +0900

    @Component 스캔

--- Spring/토비의 스프링/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ Spring/토비의 스프링/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,9 +3,11 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
+@RestController
 @RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
