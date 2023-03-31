0cb66aba2bf246e0bb3e016af837d6317e23d88f
박성민 <psm7335@naver.com>
2023-03-31 22:57:18 +0900

    Bean의 생명주기 메소드

--- Spring/토비의 스프링/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ Spring/토비의 스프링/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,25 +1,25 @@
 package tobyspring.helloboot;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println(applicationContext);
     }
 
-    @GetMapping
-    @ResponseBody
+    @GetMapping("/hello")
     public String hello(String name) {
-        System.out.println("name = " + name);
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
